d5743f44358602915af4ad01dc29ac37b0a2841c
Valdemar <valdemar.pereira@gmail.com>
2018-04-05 22:47:56 +0100

    Working on the installation... ugly commit, but I need to work on another pc

--- hipchatpolladdon/src/main/java/com/valdemar/Application.java
+++ hipchatpolladdon/src/main/java/com/valdemar/Application.java
@@ -1,7 +1,9 @@
 package com.valdemar;
 
+import com.valdemar.service.TokenStore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
@@ -9,5 +11,10 @@ public class Application {
 
     public static void main(String... args){
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public TokenStore tokenStore(){
+        return new TokenStore();
     }
 }
