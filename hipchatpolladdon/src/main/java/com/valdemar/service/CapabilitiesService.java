a13e717e62c3db0fc716313c79d95a77186f8301
Valdemar Pereira <valdemar.pereira@kindredgroup.com>
2018-04-10 11:19:30 +0100

    Remove un-use imports

--- hipchatpolladdon/src/main/java/com/valdemar/service/CapabilitiesService.java
+++ hipchatpolladdon/src/main/java/com/valdemar/service/CapabilitiesService.java
@@ -2,10 +2,7 @@ package com.valdemar.service;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
-import retrofit2.http.Path;
 import retrofit2.http.Url;
-
-import java.util.List;
 
 public interface CapabilitiesService {
     @GET
